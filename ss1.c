b13cf918f8d6706a58688bcf68548bda6db5a882
Nathan Pugh CS2022 <nathan.pugh.2021@uni.strath.ac.uk>
2024-01-23 12:28:39 +0000

    Add new file

---
+++ ss1.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_INPUT_LENGTH 512
+
+void ReadingUserInput(){
+    char UserInput[MAX_INPUT_LENGTH + 1];
+    char* token;
+    char* tokenArray[256];
+
+do {
+    printf(">");
+    fgets(UserInput, sizeof(UserInput), stdin);
+    
+    //removes newline caused by hitting <enter> 
+    int len = strlen(UserInput);
+    if(len > 0 && UserInput[len -1] == '\n'){
+        UserInput[len - 1] = '\0';
+    }
+
+    
+
+    //parses a line of user input
+    token = strtok(UserInput, " \t|<>;&");
+    if (token == NULL){
+        tokenArray[0] = "";
+        continue;
+        }
+    tokenArray[0] = token;
+     int i = 1;
+    while(token != NULL){
+    printf("\"%s\" \n", token);
+    tokenArray[i] = token;
+    i++; 
+    token = strtok(NULL, " \t|<>;&");
+}    
+
+
+
+//repeats the above lines until the first 'token' in an inputted line is exit    
+} while(strcmp("exit", tokenArray[0]) != 0);
+}
+
+
+int main(){
+    ReadingUserInput();
+    return 0;
+}
